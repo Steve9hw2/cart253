825bb858b484eb0afa4beafb760636a3c40fc26d
Steve9hw2 <70410761+Steve9hw2@users.noreply.github.com>
2020-09-16 14:53:25 -0400

    Drawing: Colour, Transparency, NoStroke

--- experiments/p5-project-drawing/js/script.js
+++ experiments/p5-project-drawing/js/script.js
@@ -13,7 +13,10 @@ function setup() {
 
   background(0, 125, 255);
 
-  rect(50,50,250,250,25,25,25,25)
+  noStroke();
+  fill(160,200,255, 100);
+
+  rect(50,50,250,250,25,25,25,25);
 
   ellipseMode(CORNER);
   ellipse(250,250,100,100);
