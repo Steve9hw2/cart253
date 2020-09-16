6b73dc91277c9e6f624a8372d0fcd0dd8bc075b3
Steve9hw2 <70410761+Steve9hw2@users.noreply.github.com>
2020-09-16 14:15:34 -0400

    Drawing: Tested basic shapes.

--- experiments/p5-project-drawing/js/script.js
+++ experiments/p5-project-drawing/js/script.js
@@ -9,7 +9,16 @@ Here is a description of this template p5 project.
 //
 // Description of setup() goes here.
 function setup() {
+  createCanvas(500, 500);
+
   background(0, 125, 255);
+
+  rect(50,50,250,250,25,25,25,25)
+
+  ellipseMode(CORNER);
+  ellipse(250,250,100,100);
+  ellipse(250,250,80,80);
+
 }
 
 // draw()
