ddbb272cd91dcc205c7d7be294b1f54770ed1362
Steve9hw2 <70410761+Steve9hw2@users.noreply.github.com>
2020-09-16 13:27:46 -0400

    Drawing experiments commence!

---
+++ experiments/p5-project-drawing/js/script.js
@@ -0,0 +1,20 @@
+/**************************************************
+Template p5 project
+Pippin Barr
+
+Here is a description of this template p5 project.
+**************************************************/
+
+// setup()
+//
+// Description of setup() goes here.
+function setup() {
+  background(0, 125, 255);
+}
+
+// draw()
+//
+// Description of draw() goes here.
+function draw() {
+
+}
