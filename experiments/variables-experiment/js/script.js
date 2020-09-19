a6b07b7242660866572d92326f4f5ea2271aa9cc
Steve9hw2 <70410761+Steve9hw2@users.noreply.github.com>
2020-09-19 13:25:38 -0400

    3-6: Introduced random numbers.
    circle go disco ball haha

--- experiments/variables-experiment/js/script.js
+++ experiments/variables-experiment/js/script.js
@@ -13,6 +13,9 @@ let circle = {
   y:0,
   size:100,
   speed:2,
+  fillr:0,
+  fillg:0,
+  fillb:0,
 };
 
 // setup()
@@ -28,9 +31,15 @@ function setup() {
 function draw() {
     backgroundHue += backgroundBrightener
     background(backgroundHue);
+
+    fill(circle.fillr,circle.fillg,circle.fillb);
     ellipse(circle.x,circle.y,circle.size);
     circle.y += circle.speed;
 
+    circle.fillr = random(10,255);
+    circle.fillg = random(10,255);
+    circle.fillb = random(10,255);
+    console.log(`circle.fillr: ${circle.fillr}, circle.fillb: ${circle.fillb}, circle.fillg: ${circle.fillg}`)
     console.log(`circle.x: ${circle.x}, circle.y: ${circle.y}, circle.size: ${circle.size}, circle.speed: ${circle.speed}`);
     //console.log("circle.x: " + circle.x);
   }
