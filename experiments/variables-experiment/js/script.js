d12cce3dcbb5e9498948d62c05e196ca2a3ad882
Steve9hw2 <70410761+Steve9hw2@users.noreply.github.com>
2020-09-19 10:19:02 -0400

    3-1 Built-in variables

--- experiments/variables-experiment/js/script.js
+++ experiments/variables-experiment/js/script.js
@@ -1,6 +1,6 @@
 /**************************************************
-Template p5 project
-Pippin Barr
+Variables Experiment
+Steve Berthiaume
 
 Here is a description of this template p5 project.
 **************************************************/
@@ -9,12 +9,14 @@ Here is a description of this template p5 project.
 //
 // Description of setup() goes here.
 function setup() {
-
+  createCanvas(windowWidth,windowHeight);
 }
 
 // draw()
 //
 // Description of draw() goes here.
 function draw() {
-
+    background(0,125,225);
+    rectMode(CENTER);
+    rect(mouseX,mouseY,100,100);
 }
