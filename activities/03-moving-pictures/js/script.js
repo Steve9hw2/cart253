fbb486b857b517ffbcc79e90636c5baaa22913a8
Steve9hw2 <70410761+Steve9hw2@users.noreply.github.com>
2020-09-19 13:56:20 -0400

    Activity 03 begins.

---
+++ activities/03-moving-pictures/js/script.js
@@ -0,0 +1,28 @@
+/**************************************************
+Activity 03 | Moving Pictures
+Steve Berthiaume
+
+This is my interpretation of the guidelines for
+Pippin Barr's assigned Activity 03 for CART 253.
+**************************************************/
+
+let background = {
+  r:0,
+  g:0,
+  b:0,
+};
+
+// setup()
+//
+// Description of setup() goes here.
+function setup() {
+    createCanvas(500,500);
+
+}
+
+// draw()
+//
+// Description of draw() goes here.
+function draw() {
+
+}
