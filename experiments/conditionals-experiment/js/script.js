112b3080f0e20992e82114a2b881b1e3725c86d3
Steve9hw2 <70410761+Steve9hw2@users.noreply.github.com>
2020-09-26 11:04:05 -0400

    4-2 - Logic Operators
    interesting discrepancy between js and lua. never fully realized how simplified the operators were in lua since they were written out in plain text. gonna need some time to adapt to ||.

--- experiments/conditionals-experiment/js/script.js
+++ experiments/conditionals-experiment/js/script.js
@@ -14,7 +14,7 @@ let circle = {
   x:100,
   y:250,
   size:100,
-  speed:5,
+  speed:1,
 }
 
 function setup() {
@@ -29,24 +29,24 @@ function draw() {
 
   circle.x += circle.speed;
 
-  if (circle.x > width) {
-    circle.speed = -circle.speed;
+fill(255);
+  if (mouseX < width/3 && circle.x < 2 * width/3) {
+    fill(255,0,0);
+  }
+  else if (mouseX < 2 * width/3) {
+    fill(0,255,0);
+  }
+  else {
+    fill(0,0,255);
   }
 
-  if (circle.x < 0) {
-    circle.speed = -circle.speed;
-  }
 
-  if (mouseY < height/2) {
-    fill(255,0,0)
-  }
 
-  if (mouseY > height/2 ) {
-    fill(0,0,255)
-  }
-  
   ellipse(circle.x,circle.y,circle.size);
 }
 
 // js == should be ===
 // js ~= should be !==
+// js and should be &&
+// js or should be ||
+// js not should be ! placed before the condition
