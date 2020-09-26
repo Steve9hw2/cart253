a76aff342bae0654572dabfab57d7c2da4e1dd87
Steve9hw2 <70410761+Steve9hw2@users.noreply.github.com>
2020-09-26 13:18:16 -0400

    4-7 : Intermediate Drawing

--- experiments/conditionals-experiment/js/script.js
+++ experiments/conditionals-experiment/js/script.js
@@ -6,17 +6,9 @@ Conditionals. Not much more to say.
 (I love these things)
 **************************************************/
 
-let circle = {
-  x:0,
-  y:250,
-  size:100,
-  vx:1,
-  vy:1,
-  ax:0,
-  ay:0,
-  acceleration:0.1,
-  maxSpeed:3,
-}
+
+let angle = 0;
+let rectScale = 0;
 
 function setup() {
   createCanvas(500,500)
@@ -25,29 +17,17 @@ function setup() {
 function draw() {
   background(55,20,150)
 
-  if (mouseX < circle.x) {
-    circle.ax = -circle.acceleration;
-  }
-  else {
-    circle.ax = +circle.acceleration;
-  }
+  push();
+  fill(0,0,255);
+  rectMode(CENTER);
+  translate(width/2,height/2);
+  rotate(angle);
+  scale(rectScale);
+  rect(0,0,100,100);
+  pop();
 
-  if (mouseY < circle.y) {
-    circle.ay = -circle.acceleration;
-  }
-  else {
-    circle.ay = +circle.acceleration;
-  }
-
-  circle.vx += circle.ax;
-  circle.vx = constrain(circle.vx,-circle.maxSpeed,circle.maxSpeed);
-  circle.vy += circle.ay;
-  circle.vy = constrain(circle.vy,-circle.maxSpeed,circle.maxSpeed);
-
-  circle.x += circle.vx;
-  circle.y += circle.vy;
-
-  ellipse(circle.x,circle.y,circle.size);
+  angle += 0.01;
+  rectScale += 0.01;
 }
 
 
