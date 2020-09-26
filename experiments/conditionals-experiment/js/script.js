1b3ad64f8b111920a30c61950c00ed13acb4f700
Steve9hw2 <70410761+Steve9hw2@users.noreply.github.com>
2020-09-26 10:40:59 -0400

    4-1 Conditionals

--- experiments/conditionals-experiment/js/script.js
+++ experiments/conditionals-experiment/js/script.js
@@ -1,20 +1,52 @@
 /**************************************************
-Template p5 project
-Pippin Barr
+Conditionals Experiment
+Steve Berthiaume
 
-Here is a description of this template p5 project.
+Conditionals. Not much more to say.
+(I love these things)
 **************************************************/
 
 // setup()
 //
 // Description of setup() goes here.
-function setup() {
+let backgrounddShade = 0;
+let circle = {
+  x:100,
+  y:250,
+  size:100,
+  speed:5,
+}
 
+function setup() {
+  createCanvas(500,500)
 }
 
 // draw()
 //
 // Description of draw() goes here.
 function draw() {
+  background(backgrounddShade)
 
+  circle.x += circle.speed;
+
+  if (circle.x > width) {
+    circle.speed = -circle.speed;
+  }
+
+  if (circle.x < 0) {
+    circle.speed = -circle.speed;
+  }
+
+  if (mouseY < height/2) {
+    fill(255,0,0)
+  }
+
+  if (mouseY > height/2 ) {
+    fill(0,0,255)
+  }
+  
+  ellipse(circle.x,circle.y,circle.size);
 }
+
+// js == should be ===
+// js ~= should be !==
