c8882d824e0992490c2905e7b1ffd83981d671c3
Steve9hw2 <70410761+Steve9hw2@users.noreply.github.com>
2020-09-26 13:22:30 -0400

    Activity 04 Begins

---
+++ activities/04-dodging-covid/js/script.js
@@ -0,0 +1,20 @@
+/**************************************************
+Activity 04- Dodging Covid-19
+Steve Berthiaume
+
+Because it's a very very scary
+**************************************************/
+
+// setup()
+//
+// Description of setup() goes here.
+function setup() {
+
+}
+
+// draw()
+//
+// Description of draw() goes here.
+function draw() {
+
+}
