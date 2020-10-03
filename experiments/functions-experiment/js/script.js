9e08724e3c8fde0c7a40fa372728edfbdfbac315
Steve9hw2 <70410761+Steve9hw2@users.noreply.github.com>
2020-10-03 16:38:57 -0400

    Functions with Return

--- experiments/functions-experiment/js/script.js
+++ experiments/functions-experiment/js/script.js
@@ -7,21 +7,20 @@ Here is a description of this template p5 project.
 
 function setup() {
   createCanvas(500,500);
+
+  let hotCelsius = toCelsius(100);
+  console.log(`100 degrees Fahrenheit is ${hotCelsius} degrees Celsius.`)
+
+  let coldCelsius = toCelsius(10);
+  console.log(`10 degrees Fahrenheit is ${coldCelsius} degrees Celsius.`)
 }
 
 function draw() {
   background(0);
 
-  parallels(50,100,1,100,40,10);
-  parallels(50,400,1,100,40,10);
 }
 
-function parallels(x,y,lineWidth,lineHeight,numLines,lineSpacing) {
-  for(let i = 0; i < numLines; i++) {
-    noStroke();
-    fill(255);
-    rectMode(CENTER);
-    rect(x,y,lineWidth,lineHeight);
-    x += lineSpacing;
-  }
+function toCelsius(f) {
+  let c = (f-32) * 5/9;
+  return c;
 }
