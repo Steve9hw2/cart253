d638f8119c095f9633e5c79b720bbf862db17e8d
Steve9hw2 <70410761+Steve9hw2@users.noreply.github.com>
2020-10-03 13:02:37 -0400

    Functions with Parameters

--- experiments/functions-experiment/js/script.js
+++ experiments/functions-experiment/js/script.js
@@ -5,16 +5,23 @@ Steve Berthiaume
 Here is a description of this template p5 project.
 **************************************************/
 
-// setup()
-//
-// Description of setup() goes here.
 function setup() {
-
+  createCanvas(500,500);
 }
 
-// draw()
-//
-// Description of draw() goes here.
 function draw() {
+  background(0);
 
+  parallels(50,100,1,100,40,10);
+  parallels(50,400,1,100,40,10);
+}
+
+function parallels(x,y,lineWidth,lineHeight,numLines,lineSpacing) {
+  for(let i = 0; i < numLines; i++) {
+    noStroke();
+    fill(255);
+    rectMode(CENTER);
+    rect(x,y,lineWidth,lineHeight);
+    x += lineSpacing;
+  }
 }
