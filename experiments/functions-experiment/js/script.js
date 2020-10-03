ade6a9b79a0ba2d5b7a957e3037a471fa738d02b
Steve9hw2 <70410761+Steve9hw2@users.noreply.github.com>
2020-10-03 17:07:51 -0400

    States

--- experiments/functions-experiment/js/script.js
+++ experiments/functions-experiment/js/script.js
@@ -4,23 +4,61 @@ Steve Berthiaume
 
 Here is a description of this template p5 project.
 **************************************************/
+let circle = {
+  x:0,
+  y:250,
+  size:100,
+  vx:0,
+  vy:0,
+  speed:2,
+};
+
+let state = `title`; // Possible states are: title, anim, ending
+
 
 function setup() {
   createCanvas(500,500);
-
-  let hotCelsius = toCelsius(100);
-  console.log(`100 degrees Fahrenheit is ${hotCelsius} degrees Celsius.`)
-
-  let coldCelsius = toCelsius(10);
-  console.log(`10 degrees Fahrenheit is ${coldCelsius} degrees Celsius.`)
+  circle.vx = circle.speed
+  textSize(32);
+  textAlign(CENTER,CENTER);
 }
 
 function draw() {
   background(0);
 
+  if (state === `title`) {
+    title();
+  }
+  else if (state === `animation`) {
+    anim();
+  }
+  else if (state === `ending`) {
+    ending();
+  }
 }
 
-function toCelsius(f) {
-  let c = (f-32) * 5/9;
-  return c;
+function title() {
+  // Title
+  fill(255);
+  text(`Life`,width/2,height/2);
+}
+function anim() {
+  // Anim
+  circle.x += circle.vx
+  circle.y += circle.vy
+
+  ellipse(circle.x,circle.y,circle.size)
+  if (circle.x > width) {
+    state = `ending`;
+  }
+}
+function ending() {
+  // Ending
+  fill(127);
+  text(`it's over.`,width/2,height/2);
+}
+function keyPressed() {
+  if (state === `title`) {
+  state =  `animation`;
+}
 }
