be0251398eeb9ea40c70202ab95878e6de787103
Steve9hw2 <70410761+Steve9hw2@users.noreply.github.com>
2020-10-04 18:49:53 -0400

    P1: Started project one.

---
+++ projects/Project01-Simulation/js/script.js
@@ -0,0 +1,20 @@
+/**************************************************
+Project 01: Simulation
+Steve Berthiaume
+
+This is my take on the first project for CART 253.
+**************************************************/
+
+// setup()
+//
+// Description of setup() goes here.
+function setup() {
+
+}
+
+// draw()
+//
+// Description of draw() goes here.
+function draw() {
+
+}
