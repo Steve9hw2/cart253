e31716ab824854e23cb036f57811cceb981c1287
Steve9hw2 <70410761+Steve9hw2@users.noreply.github.com>
2020-10-06 13:50:48 -0400

    P1: Assets Imported.
    Pictures and sounds imported, and a skeleton of the state code has been implemented.

--- projects/Project01-Simulation/js/script.js
+++ projects/Project01-Simulation/js/script.js
@@ -9,12 +9,12 @@ This is my take on the first project for CART 253.
 //
 // Description of setup() goes here.
 function setup() {
-
+    createCanvas(2400,1200);
 }
 
 // draw()
 //
 // Description of draw() goes here.
 function draw() {
-
+    background(0);
 }
