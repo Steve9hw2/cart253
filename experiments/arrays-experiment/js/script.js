8608ce4b17893a49ac373eac8c33a54a64891584
Steve9hw2 <70410761+Steve9hw2@users.noreply.github.com>
2020-10-16 11:51:23 -0400

    Arrays: Implemented
    *ohhhhhhhhhhhhhhhhhhh*

--- experiments/arrays-experiment/js/script.js
+++ experiments/arrays-experiment/js/script.js
@@ -6,22 +6,17 @@ Here is a description of this template p5 project.
 **************************************************/
 "use strict";
 
-//fish!
-let fish1;
-let fish2;
-let fish3;
-let fish4;
-
+let school = []; //fish
+let schoolSize = 4;
 // setup()
 //
 // Description of setup() goes here.
 function setup() {
   createCanvas(600, 600);
-
-  fish1 = createFish(random(0,width), random(0,height));
-    fish2 = createFish(random(0,width), random(0,height));
-      fish3 = createFish(random(0,width), random(0,height));
-        fish4 = createFish(random(0,width), random(0,height));
+  for (let i = 0; i < schoolSize; i++) {
+    let fish = createFish(random(0,width),random(0,height));
+    school.push(fish);
+  }
 }
 
 function createFish(x,y) {
@@ -41,15 +36,11 @@ function createFish(x,y) {
 // Description of draw() goes here.
 function draw() {
   background(0);
-  moveFish(fish1);
-  moveFish(fish2);
-  moveFish(fish3);
-  moveFish(fish4);
-
-  displayFish(fish1);
-  displayFish(fish2);
-  displayFish(fish3);
-  displayFish(fish4);
+  for (let i = 0; i < school.length; i++) {
+    moveFish(school[i]);
+    displayFish(school[i]);
+  }
+  print(school.length);
 }
 
 function moveFish(fish) {
@@ -70,4 +61,9 @@ function displayFish(fish) {
   noStroke();
   ellipse(fish.x,fish.y,fish.size);
   pop();
+}
+
+function mousePressed() {
+  let fish = createFish(mouseX,mouseY);
+  school.push(fish);
 }
