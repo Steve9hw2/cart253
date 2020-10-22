c0377c1a12af01c9b9126980f8b808f66c87c0ba
Steve9hw2 <70410761+Steve9hw2@users.noreply.github.com>
2020-10-22 13:45:48 -0400

    E4: Begins!

---
+++ exercises/exercise4/js/script.js
@@ -0,0 +1,21 @@
+/**************************************************
+Exercise 4
+Steve Berthiaume
+
+Age of Aquariums
+**************************************************/
+"use strict;"
+
+// setup()
+//
+// Description of setup() goes here.
+function setup() {
+
+}
+
+// draw()
+//
+// Description of draw() goes here.
+function draw() {
+
+}
