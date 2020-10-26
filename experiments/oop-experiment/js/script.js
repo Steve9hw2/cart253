b5857830216f068cb1132f9456ca47bfb459aacb
Steve9hw2 <70410761+Steve9hw2@users.noreply.github.com>
2020-10-26 11:40:26 -0400

    OOP: Added OOP
    For whatever reason my live server never worked for displaying this, before and after OOP, so i just tried to follow along and understand the theory behind it.

--- experiments/oop-experiment/js/script.js
+++ experiments/oop-experiment/js/script.js
@@ -19,56 +19,17 @@ let garden = {
 // setup()
 function setup() {
   createCanvas(600,600);
-
   for (let i = 0; 1 < garden.numFlowers; i++) {
-    let flower = createFlower();
+    let flower = new Flower();
     garden.flowers.push(flower);
   }
 }
 
-function createFlower() {
-  let flower = {
-    x: random(0,width),
-    y: random(0,height),
-    size:50,
-    stemLength:75,
-    stemThickness:10,
-    petalThickness:10,
-    stemColor: {
-      r:50,
-      g:150,
-      b:50,
-    },
-    petalColor: {
-      r:200,
-      g:50,
-      b:50,
-    },
-    centreColor: {
-      r:50,
-      g:0,
-      b:0,
-    }
-  };
-  return flower;
-}
-
 // draw()
 function draw() {
+  background(garden.grassColor.r,garden.grassColor.g,garden.grassColor.b);
   for (let i = 0; i < garden.flowers.length; i++) {
     let flower = garden.flowers[i];
-    displayFlower(flower);
+    flower.display();
   }
-}
-
-function displayFlower(flower) {
-  push();
-  strokeWeight(flower.stemThickness);
-  stroke(flower.stemColor.r,flower.stemColor.g,flower.stemColor.b);
-  line(flower.x , flower.y, flower.x, flower.y + flower.stemLength);
-  strokeWeight(flower.petalThickness);
-  fill(flower.centreColor.r,flower.centreColor.g,flower.centreColor.b);
-  stroke(flower.petalColor.r,flower.petalColor.g,flower.petalColor.b);
-  ellipse(flower.x,flower.y,flower.size);
-  pop();
 }
