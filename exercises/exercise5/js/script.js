3402bcde0ea755f1c254a46ca6f649453a6f8eef
Steve9hw2 <70410761+Steve9hw2@users.noreply.github.com>
2020-10-29 13:33:08 -0400

    E5: Implemented States
    As the comment title says, really.

--- exercises/exercise5/js/script.js
+++ exercises/exercise5/js/script.js
@@ -6,6 +6,7 @@ OOP Activity. I dunno, it's not given a name.
 **************************************************/
 "use strict";
 
+let state = `game` // game, none, bad, decent, good, amazing, impossible
 let gravity = 0.0025;
 let paddle;
 let goal;
@@ -34,31 +35,34 @@ function setup() {
 // draw()
 function draw() {
   background(0);
+  if (state === `game`) {
   activeBalls.value = 0;
-
   paddle.display();
   goal.anim();
   goal.display();
   counter.display();
-
-  for(let i = 0; i < balls.length; i++) {
-    let ball = balls[i];
-    if (ball.active) {
-      ball.gravity(gravity);
-      ball.move();
-      ball.bounce(paddle);
-      ball.point(goal);
-      ball.display();
-      activeBalls.value ++
-  }
-    if (!ball.active && ball.score) {
-      ball.vanish();
-      ball.score = false;
-      counter.score ++;
-    }
-  }
-
+  ballAction();
   activeBalls.display();
+  end();
+  }
+  else if (state === `none`) {
+
+  }
+  else if (state === `bad`) {
+
+  }
+  else if (state === `decent`) {
+
+  }
+  else if (state === `good`) {
+
+  }
+  else if (state === `amazing`) {
+
+  }
+  else if (state === `impossible`) {
+
+  }
 }
 
 function keyPressed() {
@@ -79,4 +83,46 @@ function keyPressed() {
       buffer = true;
       paddle.x = 1000;
     }
+} // Player input
+
+function ballAction() {
+  for(let i = 0; i < balls.length; i++) {
+    let ball = balls[i];
+    if (ball.active) {
+      ball.gravity(gravity);
+      ball.move();
+      ball.bounce(paddle);
+      ball.point(goal);
+      ball.display();
+      activeBalls.value ++
+  }
+    if (!ball.active && ball.score) {
+      ball.vanish();
+      ball.score = false;
+      counter.score ++;
+    }
+  }
+} // All possible ball interactions.
+
+function end() {
+  if (activeBalls.value === 0) {
+    if (counter.score === 0) {
+      state = `none`;
+    }
+    else if (counter.score === 1) {
+      state = `bad`;
+    }
+    else if (counter.score === 2) {
+      state = `decent`;
+    }
+    else if (counter.score === 3) {
+      state = `good`;
+    }
+    else if (counter.score > 3 && counter.score < 10) {
+      state = `amazing`;
+    }
+    else if (counter.score > 9) {
+      state = `impossible`;
+    }
+  }
 }
