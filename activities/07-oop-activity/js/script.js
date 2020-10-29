8afa89176d62c3b1ec17962d9a1c9278aa4dfaca
Steve9hw2 <70410761+Steve9hw2@users.noreply.github.com>
2020-10-29 12:04:17 -0400

    A7: Done!

--- activities/07-oop-activity/js/script.js
+++ activities/07-oop-activity/js/script.js
@@ -2,16 +2,43 @@
 OOP Activity
 Steve Berthiaume
 
-Here is a description of this template p5 project.
+OOP Activity. I dunno, it's not given a name.
 **************************************************/
 "use strict";
 
+let gravity = 0.0025;
+let paddle;
+let balls = [];
+let numBalls = 12;
+
 // setup()
 function setup() {
+  createCanvas(windowWidth,windowHeight);
 
+  paddle = new Paddle(300,20);
+
+  for(let i = 0; i < numBalls; i++) {
+    let x = random(0,width);
+    let y = random(-1200,-100);
+    let ball = new Ball(x,y);
+    balls.push(ball);
+  }
 }
 
 // draw()
 function draw() {
+  background(0);
 
+  paddle.move();
+  paddle.display();
+
+  for(let i = 0; i < balls.length; i++) {
+    let ball = balls[i];
+    if (ball.active) {
+      ball.gravity(gravity);
+      ball.move();
+      ball.bounce(paddle);
+      ball.display();
+  }
+  }
 }
