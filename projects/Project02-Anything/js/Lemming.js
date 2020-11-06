745959133695fe8b11b77443adde80b5acb8acf7
Steve9hw2 <70410761+Steve9hw2@users.noreply.github.com>
2020-11-05 22:14:09 -0500

    P2: Prototype - Section 1-1 & 1-2

--- projects/Project02-Anything/js/Lemming.js
+++ projects/Project02-Anything/js/Lemming.js
@@ -4,6 +4,16 @@ class Lemming {
     this.x = 0;
     this.y = 0;
     this.variation = variation;
+    this.moving = false;
+    this.falling = false;
+    this.collide = false;
+    this.dead = false;
+    this.safe = false;
+    this.vy = 1.2;
+    this.ay = 0.5;
+    this.maxGrav = 10;
+    this.number = undefined;
+    this.scream = false;
     if (variation < 0) {
       variation = 0;
     }
@@ -12,31 +22,52 @@ class Lemming {
     }
     switch(variation) {
       case 0:
-      this.speed = 1.2;
+      this.speed = 1;
       break;
       case 1:
-      this.speed = 1.3;
+      this.speed = 1;
       break;
       case 2:
-      this.speed = 0.9;
+      this.speed = 1;
       break;
       case 3:
-      this.speed = 0.7;
+      this.speed = 1;
       break;
       case 4:
       this.speed = 1;
       break;
       case 5:
-      this.speed = 1.1;
+      this.speed = 1;
       break;
       case 6:
-      this.speed = 1.4;
+      this.speed = 1;
       break;
       case 7:
-      this.speed = 1.5;
+      this.speed = 1;
       break;
     }
     this.variant = variation;
+  }
+
+  advance(startY) {
+    if (!this.moving && !this.collide && !this.safe && !this.falling && !this.dead) {
+      this.x = -100 - 80*this.number ;
+      this.y = startY;
+    }
+    else if (this.moving && !this.collide && !this.safe && !this.falling && !this.dead) {
+      this.x += this.speed;
+    }
+  }
+
+  gravity() {
+    this.vy += this.ay;
+    this.vy = constrain(this.vy,-this.maxGrav,this.maxGrav);
+    this.y += this.vy
+    this.falling = true;
+  }
+
+  climb(y) {
+    this.y += y;
   }
 
 }
