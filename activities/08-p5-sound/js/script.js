903841cbadda3d9aec017cd04a846c2010493309
Steve9hw2 <70410761+Steve9hw2@users.noreply.github.com>
2020-11-12 13:19:24 -0500

    A8: Activity Finished

--- activities/08-p5-sound/js/script.js
+++ activities/08-p5-sound/js/script.js
@@ -7,12 +7,32 @@ Here is a description of this template p5 project.
 
 "use strict";
 
+let balls = [];
+let notes = [`F3`,`G3`,`Ab4`,`Bb4`,`C4`,`Db4`,`Eb4`,`F4`];
+
 // setup()
 function setup() {
-
+  createCanvas(600,600);
+  userStartAudio();
 }
 
 // draw()
 function draw() {
+  background(0);
+  for(let i = 0; i < balls.length; i++) {
+    let ball = balls[i];
+    ball.move();
+    ball.bounce();
+    ball.display();
+  }
+}
 
+function mousePressed() {
+  createBall(mouseX,mouseY);
+}
+
+function createBall(x,y) {
+  let note = random(notes);
+  let ball = new Ball(x,y,note);
+  balls.push(ball);
 }
