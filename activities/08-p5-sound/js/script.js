1954ab4c83d32c4669206b4425edda7bcfe7f3da
Steve9hw2 <70410761+Steve9hw2@users.noreply.github.com>
2020-11-12 12:45:16 -0500

    A8: p5.sound Activity Begins

---
+++ activities/08-p5-sound/js/script.js
@@ -0,0 +1,18 @@
+/**************************************************
+P5 Sound Activity
+Steve Berthiaume
+
+Here is a description of this template p5 project.
+**************************************************/
+
+"use strict";
+
+// setup()
+function setup() {
+
+}
+
+// draw()
+function draw() {
+
+}
